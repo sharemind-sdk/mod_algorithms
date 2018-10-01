78a8716466cab5ae9f738a7d5688133a210df98e
Jaak Ristioja <jaak.ristioja@cyber.ee>
2018-10-01 17:03:26 +0300

    Sync with libsortnetwork changes.

--- src/SortingNetworkGenerator.h
+++ src/SortingNetworkGenerator.h
@@ -34,127 +34,86 @@ class __attribute__ ((visibility("internal"))) SortingNetworkGenerator {
 
 private: /* Types: */
 
-    class SortingNetwork {
+    using Network = sharemind::SortingNetwork::Network;
+    using Stage = sharemind::SortingNetwork::Stage;
+
+    class SortingNetwork: private Network {
 
     public: /* Methods: */
 
         /**
           \brief Generates the sorting network.
           \note This takes significant time for larger inputs.
-          \bug There's no way to tell between invalid argument to
-               sn_network_create_bitonic_mergesort and out of memory errors.
         */
-        SortingNetwork(const size_t elements)
-            : m_sortingNetwork(sn_network_create_bitonic_mergesort(elements)) {}
+        SortingNetwork(std::size_t numInputs)
+            : Network(Network::makeBitonicMergeSort(numInputs))
+            , m_serializationSize(
+                [this]() {
+                    // Start with the number of stages as a single number:
+                    std::size_t r = 1u;
+                    // Second, we store each stage separately:
+                    for (auto const & stage : stages()) {
+                        // For each stage, we store its size as a single number:
+                        ++r;
+                        /* Then we store all the first indices in the pairs and
+                           then the second ones: */
+                        r += 4u * stage.numComparators();
+                    }
+                    return r;
+                }())
+        {}
 
-        ~SortingNetwork() noexcept {
-            // Destroy the sorting network
-            if (m_sortingNetwork)
-                sn_network_destroy(m_sortingNetwork);
-        }
+        std::size_t serializedSize() const noexcept
+        { return m_serializationSize; }
 
-        /**
-          \bug There's no way to tell between invalid argument to
-               sn_network_create_bitonic_mergesort and out of memory errors.
-        */
-        inline bool isValid() const noexcept
-        { return m_sortingNetwork != nullptr; }
-
-        inline size_t getNumberOfStages() const noexcept
-        { return SN_NETWORK_STAGE_NUM(m_sortingNetwork); }
-
-        inline size_t getNumberOfPairsInStage(const size_t stage) const noexcept
-        { return SN_STAGE_COMP_NUM(getStage(stage)); }
-
-        /*
-         * For efficiency, it does not make sense to wrap the internal data
-         * structures of the sorting network so we expose them here.
-         */
-        inline sn_stage_s * getStage(const size_t stage) const noexcept
-        { return SN_NETWORK_STAGE_GET(m_sortingNetwork, stage); }
-
-        inline size_t serializedSize() const noexcept {
-            if (m_serializationSize != 0u)
-                return m_serializationSize;
-            // First, we would store the number of stages as a single number
-            m_serializationSize = 1u;
-            // Second, we store each stage separately
-            for (size_t s = 0u; s < getNumberOfStages(); s++) {
-                // For each stage, we store its size as a single number
-                m_serializationSize++;
-                // Then we store all the first indices in the pairs
-                // and then the second ones
-                m_serializationSize += 4u * getNumberOfPairsInStage(s);
-            }
-            return m_serializationSize;
-        }
-
-        inline void serialize(uint64_t * const ptr) const noexcept {
-            size_t offset = 0u;
-
+        void serialize(std::uint64_t * ptr) const noexcept {
             // First, we store the number of stages
-            static_assert(std::numeric_limits<size_t>::max()
-                          == std::numeric_limits<uint64_t>::max(),
+            static_assert(std::numeric_limits<std::size_t>::max()
+                          == std::numeric_limits<std::uint64_t>::max(),
                           "Platform not supported!");
-            ptr[offset++] = getNumberOfStages();
+            (*ptr) = numStages();
             // Second, we store each stage separately
-            for (size_t s = 0u; s < getNumberOfStages(); s++) {
+            for (auto const & stage : stages()) {
                 // For each stage, we store its size as a single number
-                const size_t pairsInStage = getNumberOfPairsInStage(s);
-                ptr[offset++] = pairsInStage;
-
-                // Extract the stage
-                sn_stage_t * const stage = getStage(s);
-                sn_comparator_t * comp = nullptr;
+                (*++ptr) = stage.numComparators();
 
                 // First we store the left sides of the pairs
-                for (size_t p = 0u; p < pairsInStage; p++) {
-                    comp = SN_STAGE_COMP_GET(stage, p);
-                    ptr[offset++] = SN_COMP_LEFT(comp);
-                }
+                for (auto const & comp : stage.comparators())
+                    (*++ptr) = comp.left();
 
                 // Then, we store the right sides of the pairs
-                for (size_t p = 0u; p < pairsInStage; p++) {
-                    comp = SN_STAGE_COMP_GET(stage, p);
-                    ptr[offset++] = SN_COMP_RIGHT(comp);
-                }
+                for (auto const & comp : stage.comparators())
+                    (*++ptr) = comp.right();
 
                 // We also store the target positions. Minima first.
-                for (size_t p = 0u; p < pairsInStage; p++) {
-                    comp = SN_STAGE_COMP_GET(stage, p);
-                    ptr[offset++] = SN_COMP_MIN(comp);
-                }
+                for (auto const & comp : stage.comparators())
+                    (*++ptr) = comp.min();
 
                 // Maxima second.
-                for (size_t p = 0u; p < pairsInStage; p++) {
-                    comp = SN_STAGE_COMP_GET(stage, p);
-                    ptr[offset++] = SN_COMP_MAX(comp);
-                }
+                for (auto const & comp : stage.comparators())
+                    (*++ptr) = comp.max();
             }
         }
 
     private: /* Fields: */
 
-        sn_network_t * const m_sortingNetwork;
-        mutable size_t m_serializationSize = 0u;
+        std::size_t const m_serializationSize = 0u;
 
     }; /* class SortingNetwork { */
 
 public: /* Methods: */
 
-    inline SortingNetwork * getCachedOrGenerateAndCacheNetwork(
-                const size_t elements)
+    SortingNetwork * getCachedOrGenerateAndCacheNetwork(
+                std::size_t const numInputs)
     {
         std::lock_guard<std::mutex> lock(m_sortingNetworkCacheMutex);
-        auto it(m_sortingNetworkCache.find(elements));
+        auto it(m_sortingNetworkCache.find(numInputs));
         if (it != m_sortingNetworkCache.cend())
             return it->second.get();
-        auto sn(sharemind::makeUnique<SortingNetwork>(elements));
-        if (!sn->isValid())
-            return nullptr;
+        auto sn(sharemind::makeUnique<SortingNetwork>(numInputs));
         auto r(sn.get());
         SHAREMIND_DEBUG_ONLY(auto const rv =)
-                m_sortingNetworkCache.emplace(elements, std::move(sn));
+                m_sortingNetworkCache.emplace(numInputs, std::move(sn));
         assert(rv.second);
         return r;
     }
@@ -162,7 +121,8 @@ public: /* Methods: */
 private: /* Fields: */
 
     mutable std::mutex m_sortingNetworkCacheMutex;
-    std::map<size_t, std::unique_ptr<SortingNetwork> > m_sortingNetworkCache;
+    std::map<std::size_t, std::unique_ptr<SortingNetwork> >
+            m_sortingNetworkCache;
 
 }; /* class SortingNetworkGenerator {*/
 
